8dc41a1d039246a96882b6115d9963ee27e86460
sdk-team <sdk-team@alibabacloud.com>
2024-08-23 17:15:10 +0000

    Generated swift 2024-06-28 for DianJin.

--- dianjin-20240628/Package.swift
+++ dianjin-20240628/Package.swift
@@ -21,7 +21,7 @@ let package = Package(
             .package(url: "https://github.com/alibabacloud-sdk-swift/openplatform-20191219", from: "1.0.0"),
             .package(url: "https://github.com/alibabacloud-sdk-swift/tea-oss-utils", from: "1.0.1"),
             .package(url: "https://github.com/alibabacloud-sdk-swift/tea-fileform", from: "1.0.3"),
-            .package(url: "https://github.com/alibabacloud-sdk-swift/darabonba-openapi", from: "1.0.6"),
+            .package(url: "https://github.com/alibabacloud-sdk-swift/darabonba-openapi", from: "1.0.7"),
             .package(url: "https://github.com/alibabacloud-sdk-swift/openapi-util", from: "1.0.1"),
             .package(url: "https://github.com/alibabacloud-sdk-swift/endpoint-util", from: "1.0.0"),
         ],
