b5d34e5a9851d799c9eb7e0415531c3a0730034d
sdk-team <sdk-team@alibabacloud.com>
2025-06-09 17:15:02 +0000

    Update API UpdateAcl: add request parameters NetworkType.

--- mse-20190531/Sources/AlibabacloudMse20190531/Client.swift
+++ mse-20190531/Sources/AlibabacloudMse20190531/Client.swift
@@ -8967,6 +8967,9 @@ open class Client : AlibabacloudOpenApi.Client {
         if (!TeaUtils.Client.isUnset(request.instanceId)) {
             query["InstanceId"] = request.instanceId ?? "";
         }
+        if (!TeaUtils.Client.isUnset(request.networkType)) {
+            query["NetworkType"] = request.networkType ?? "";
+        }
         var req: AlibabacloudOpenApi.OpenApiRequest = AlibabacloudOpenApi.OpenApiRequest([
             "query": AlibabaCloudOpenApiUtil.Client.query(query)
         ])
