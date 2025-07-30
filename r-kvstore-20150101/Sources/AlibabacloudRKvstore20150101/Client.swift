877f30f2d4aa223325f02fe6de5c92e986696681
sdk-team <sdk-team@alibabacloud.com>
2025-07-30 10:01:35 +0000

    Generated swift 2015-01-01 for R-kvstore.

--- r-kvstore-20150101/Sources/AlibabacloudRKvstore20150101/Client.swift
+++ r-kvstore-20150101/Sources/AlibabacloudRKvstore20150101/Client.swift
@@ -17,7 +17,6 @@ open class Client : AlibabacloudOpenApi.Client {
             "cn-shanghai": "r-kvstore.aliyuncs.com",
             "cn-shenzhen": "r-kvstore.aliyuncs.com",
             "cn-heyuan": "r-kvstore.aliyuncs.com",
-            "cn-guangzhou": "r-kvstore.aliyuncs.com",
             "cn-hangzhou-finance": "r-kvstore.aliyuncs.com",
             "cn-shanghai-finance-1": "r-kvstore.aliyuncs.com",
             "ap-northeast-2-pop": "r-kvstore.aliyuncs.com",
