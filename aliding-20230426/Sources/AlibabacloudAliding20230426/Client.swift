b4f874cc98eb49b4ca2807cd50362191f8efdd25
sdk-team <sdk-team@alibabacloud.com>
2026-06-30 11:30:08 +0000

    Support API DisableSceneGroupTemplate.

--- aliding-20230426/Sources/AlibabacloudAliding20230426/Client.swift
+++ aliding-20230426/Sources/AlibabacloudAliding20230426/Client.swift
@@ -5003,6 +5003,62 @@ open class Client : AlibabacloudOpenApi.Client {
     }
 
     @available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
+    public func disableSceneGroupTemplateWithOptions(_ tmpReq: DisableSceneGroupTemplateRequest, _ tmpHeader: DisableSceneGroupTemplateHeaders, _ runtime: TeaUtils.RuntimeOptions) async throws -> DisableSceneGroupTemplateResponse {
+        try TeaUtils.Client.validateModel(tmpReq)
+        var request: DisableSceneGroupTemplateShrinkRequest = DisableSceneGroupTemplateShrinkRequest([:])
+        AlibabaCloudOpenApiUtil.Client.convert(tmpReq, request)
+        var headers: DisableSceneGroupTemplateShrinkHeaders = DisableSceneGroupTemplateShrinkHeaders([:])
+        AlibabaCloudOpenApiUtil.Client.convert(tmpHeader, headers)
+        if (!TeaUtils.Client.isUnset(tmpHeader.accountContext)) {
+            headers.accountContextShrink = AlibabaCloudOpenApiUtil.Client.arrayToStringWithSpecifiedStyle(tmpHeader.accountContext, "AccountContext", "json")
+        }
+        if (!TeaUtils.Client.isUnset(tmpReq.tenantContext)) {
+            request.tenantContextShrink = AlibabaCloudOpenApiUtil.Client.arrayToStringWithSpecifiedStyle(tmpReq.tenantContext, "TenantContext", "json")
+        }
+        var body: [String: Any] = [:]
+        if (!TeaUtils.Client.isUnset(request.openConversationId)) {
+            body["OpenConversationId"] = request.openConversationId ?? "";
+        }
+        if (!TeaUtils.Client.isUnset(request.templateId)) {
+            body["TemplateId"] = request.templateId ?? "";
+        }
+        if (!TeaUtils.Client.isUnset(request.tenantContextShrink)) {
+            body["TenantContext"] = request.tenantContextShrink ?? "";
+        }
+        var realHeaders: [String: String] = [:]
+        if (!TeaUtils.Client.isUnset(headers.commonHeaders)) {
+            realHeaders = headers.commonHeaders ?? [:]
+        }
+        if (!TeaUtils.Client.isUnset(headers.accountContextShrink)) {
+            realHeaders["AccountContext"] = TeaUtils.Client.toJSONString(headers.accountContextShrink);
+        }
+        var req: AlibabacloudOpenApi.OpenApiRequest = AlibabacloudOpenApi.OpenApiRequest([
+            "headers": realHeaders as! [String: String],
+            "body": AlibabaCloudOpenApiUtil.Client.parseToMap(body)
+        ])
+        var params: AlibabacloudOpenApi.Params = AlibabacloudOpenApi.Params([
+            "action": "DisableSceneGroupTemplate",
+            "version": "2023-04-26",
+            "protocol": "HTTPS",
+            "pathname": "/dingtalk/v1/im/disableSceneGroupTemplate",
+            "method": "POST",
+            "authType": "AK",
+            "style": "ROA",
+            "reqBodyType": "formData",
+            "bodyType": "json"
+        ])
+        var tmp: [String: Any] = try await callApi(params as! AlibabacloudOpenApi.Params, req as! AlibabacloudOpenApi.OpenApiRequest, runtime as! TeaUtils.RuntimeOptions)
+        return Tea.TeaConverter.fromMap(DisableSceneGroupTemplateResponse(), tmp)
+    }
+
+    @available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
+    public func disableSceneGroupTemplate(_ request: DisableSceneGroupTemplateRequest) async throws -> DisableSceneGroupTemplateResponse {
+        var runtime: TeaUtils.RuntimeOptions = TeaUtils.RuntimeOptions([:])
+        var headers: DisableSceneGroupTemplateHeaders = DisableSceneGroupTemplateHeaders([:])
+        return try await disableSceneGroupTemplateWithOptions(request as! DisableSceneGroupTemplateRequest, headers as! DisableSceneGroupTemplateHeaders, runtime as! TeaUtils.RuntimeOptions)
+    }
+
+    @available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
     public func docBlocksDeleteWithOptions(_ tmpReq: DocBlocksDeleteRequest, _ tmpHeader: DocBlocksDeleteHeaders, _ runtime: TeaUtils.RuntimeOptions) async throws -> DocBlocksDeleteResponse {
         try TeaUtils.Client.validateModel(tmpReq)
         var request: DocBlocksDeleteShrinkRequest = DocBlocksDeleteShrinkRequest([:])
