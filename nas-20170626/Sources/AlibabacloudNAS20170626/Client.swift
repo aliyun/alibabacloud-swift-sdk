8fb59f2638a0f11de006b93a734a8847b9ddfb04
sdk-team <sdk-team@alibabacloud.com>
2026-09-11 17:24:30 +0000

    Update API CreateCpfsAccessPoint: add request parameters Tag.

--- nas-20170626/Sources/AlibabacloudNAS20170626/Client.swift
+++ nas-20170626/Sources/AlibabacloudNAS20170626/Client.swift
@@ -41,40 +41,7 @@ open class Client : AlibabacloudOpenApi.Client {
             "cn-zhangjiakou-na62-a01": "nas.aliyuncs.com",
             "cn-zhengzhou-nebula-1": "nas.aliyuncs.com",
             "eu-west-1-oxs": "nas.aliyuncs.com",
-            "rus-west-1-pop": "nas.aliyuncs.com",
-            "cn-zhongwei": "nas.cn-zhongwei.aliyuncs.com",
-            "cn-wulanchabu": "nas.cn-wulanchabu.aliyuncs.com",
-            "cn-beijing": "nas.cn-beijing.aliyuncs.com",
-            "cn-shanghai": "nas.cn-shanghai.aliyuncs.com",
-            "cn-hongkong": "nas.cn-hongkong.aliyuncs.com",
-            "cn-heyuan": "nas.cn-heyuan.aliyuncs.com",
-            "cn-zhangjiakou": "nas.cn-zhangjiakou.aliyuncs.com",
-            "cn-shenzhen": "nas.cn-shenzhen.aliyuncs.com",
-            "ap-northeast-2": "nas.ap-northeast-2.aliyuncs.com",
-            "ap-northeast-1": "nas.ap-northeast-1.aliyuncs.com",
-            "cn-chengdu": "nas.cn-chengdu.aliyuncs.com",
-            "cn-guangzhou": "nas.cn-guangzhou.aliyuncs.com",
-            "ap-southeast-1": "nas.ap-southeast-1.aliyuncs.com",
-            "ap-southeast-3": "nas.ap-southeast-3.aliyuncs.com",
-            "cn-huhehaote": "nas.cn-huhehaote.aliyuncs.com",
-            "ap-southeast-5": "nas.ap-southeast-5.aliyuncs.com",
-            "ap-southeast-6": "nas.ap-southeast-6.aliyuncs.com",
-            "ap-southeast-7": "nas.ap-southeast-7.aliyuncs.com",
-            "cn-hangzhou": "nas.cn-hangzhou.aliyuncs.com",
-            "ap-southeast-8": "nas.ap-southeast-8.aliyuncs.com",
-            "us-east-1": "nas.us-east-1.aliyuncs.com",
-            "us-southeast-1": "nas.us-southeast-1.aliyuncs.com",
-            "sa-east-1": "nas.sa-east-1.aliyuncs.com",
-            "eu-west-1": "nas.eu-west-1.aliyuncs.com",
-            "eu-west-2": "nas.eu-west-2.aliyuncs.com",
-            "us-west-1": "nas.us-west-1.aliyuncs.com",
-            "eu-central-1": "nas.eu-central-1.aliyuncs.com",
-            "na-south-1": "nas.na-south-1.aliyuncs.com",
-            "me-east-1": "nas.me-east-1.aliyuncs.com",
-            "me-central-1": "nas.me-central-1.aliyuncs.com",
-            "cn-shenzhen-finance-1": "nas.cn-shenzhen-finance-1.aliyuncs.com",
-            "cn-beijing-finance-1": "nas.cn-beijing-finance-1.aliyuncs.com",
-            "cn-shanghai-finance-1": "nas.cn-shanghai-finance-1.aliyuncs.com"
+            "rus-west-1-pop": "nas.aliyuncs.com"
         ]
         try checkConfig(config as! AlibabacloudOpenApi.Config)
         self._endpoint = try getEndpoint("nas", self._regionId ?? "", self._endpointRule ?? "", self._network ?? "", self._suffix ?? "", self._endpointMap ?? [:], self._endpoint ?? "")
@@ -861,6 +828,9 @@ open class Client : AlibabacloudOpenApi.Client {
         }
         if (!TeaUtils.Client.isUnset(request.rootDirectory)) {
             query["RootDirectory"] = request.rootDirectory!;
+        }
+        if (!TeaUtils.Client.isUnset(request.tag)) {
+            query["Tag"] = request.tag ?? [];
         }
         var req: AlibabacloudOpenApi.OpenApiRequest = AlibabacloudOpenApi.OpenApiRequest([
             "query": AlibabaCloudOpenApiUtil.Client.query(query)
@@ -2695,6 +2665,9 @@ open class Client : AlibabacloudOpenApi.Client {
         }
         if (!TeaUtils.Client.isUnset(request.regionId)) {
             query["RegionId"] = request.regionId ?? "";
+        }
+        if (!TeaUtils.Client.isUnset(request.tag)) {
+            query["Tag"] = request.tag ?? [];
         }
         var req: AlibabacloudOpenApi.OpenApiRequest = AlibabacloudOpenApi.OpenApiRequest([
             "query": AlibabaCloudOpenApiUtil.Client.query(query)
--- nas-20170626/Sources/AlibabacloudNAS20170626/Models.swift
+++ nas-20170626/Sources/AlibabacloudNAS20170626/Models.swift
@@ -2926,6 +2926,44 @@ public class CreateCpfsAccessPointRequest : Tea.TeaModel {
             }
         }
     }
+    public class Tag : Tea.TeaModel {
+        public var key: String?
+
+        public var value: String?
+
+        public override init() {
+            super.init()
+        }
+
+        public init(_ dict: [String: Any]) {
+            super.init()
+            self.fromMap(dict)
+        }
+
+        public override func validate() throws -> Void {
+        }
+
+        public override func toMap() -> [String : Any] {
+            var map = super.toMap()
+            if self.key != nil {
+                map["Key"] = self.key!
+            }
+            if self.value != nil {
+                map["Value"] = self.value!
+            }
+            return map
+        }
+
+        public override func fromMap(_ dict: [String: Any?]?) -> Void {
+            guard let dict else { return }
+            if let value = dict["Key"] as? String {
+                self.key = value
+            }
+            if let value = dict["Value"] as? String {
+                self.value = value
+            }
+        }
+    }
     public var description_: String?
 
     public var fileSystemId: String?
@@ -2933,6 +2971,8 @@ public class CreateCpfsAccessPointRequest : Tea.TeaModel {
     public var regionId: String?
 
     public var rootDirectory: CreateCpfsAccessPointRequest.RootDirectory?
+
+    public var tag: [CreateCpfsAccessPointRequest.Tag]?
 
     public override init() {
         super.init()
@@ -2961,6 +3001,13 @@ public class CreateCpfsAccessPointRequest : Tea.TeaModel {
         if self.rootDirectory != nil {
             map["RootDirectory"] = self.rootDirectory?.toMap()
         }
+        if self.tag != nil {
+            var tmp : [Any] = []
+            for k in self.tag! {
+                tmp.append(k.toMap())
+            }
+            map["Tag"] = tmp
+        }
         return map
     }
 
@@ -2979,6 +3026,19 @@ public class CreateCpfsAccessPointRequest : Tea.TeaModel {
             var model = CreateCpfsAccessPointRequest.RootDirectory()
             model.fromMap(value)
             self.rootDirectory = model
+        }
+        if let value = dict["Tag"] as? [Any?] {
+            var tmp : [CreateCpfsAccessPointRequest.Tag] = []
+            for v in value {
+                if v != nil {
+                    var model = CreateCpfsAccessPointRequest.Tag()
+                    if v != nil {
+                        model.fromMap(v as? [String: Any?])
+                    }
+                    tmp.append(model)
+                }
+            }
+            self.tag = tmp
         }
     }
 }
@@ -11383,6 +11443,44 @@ public class DescribeCpfsAccessPointMountedClientsResponse : Tea.TeaModel {
 }
 
 public class DescribeCpfsAccessPointsRequest : Tea.TeaModel {
+    public class Tag : Tea.TeaModel {
+        public var key: String?
+
+        public var value: String?
+
+        public override init() {
+            super.init()
+        }
+
+        public init(_ dict: [String: Any]) {
+            super.init()
+            self.fromMap(dict)
+        }
+
+        public override func validate() throws -> Void {
+        }
+
+        public override func toMap() -> [String : Any] {
+            var map = super.toMap()
+            if self.key != nil {
+                map["Key"] = self.key!
+            }
+            if self.value != nil {
+                map["Value"] = self.value!
+            }
+            return map
+        }
+
+        public override func fromMap(_ dict: [String: Any?]?) -> Void {
+            guard let dict else { return }
+            if let value = dict["Key"] as? String {
+                self.key = value
+            }
+            if let value = dict["Value"] as? String {
+                self.value = value
+            }
+        }
+    }
     public var accessPointId: String?
 
     public var fileSystemId: String?
@@ -11392,6 +11490,8 @@ public class DescribeCpfsAccessPointsRequest : Tea.TeaModel {
     public var pageSize: Int32?
 
     public var regionId: String?
+
+    public var tag: [DescribeCpfsAccessPointsRequest.Tag]?
 
     public override init() {
         super.init()
@@ -11422,6 +11522,13 @@ public class DescribeCpfsAccessPointsRequest : Tea.TeaModel {
         if self.regionId != nil {
             map["RegionId"] = self.regionId!
         }
+        if self.tag != nil {
+            var tmp : [Any] = []
+            for k in self.tag! {
+                tmp.append(k.toMap())
+            }
+            map["Tag"] = tmp
+        }
         return map
     }
 
@@ -11441,6 +11548,19 @@ public class DescribeCpfsAccessPointsRequest : Tea.TeaModel {
         }
         if let value = dict["RegionId"] as? String {
             self.regionId = value
+        }
+        if let value = dict["Tag"] as? [Any?] {
+            var tmp : [DescribeCpfsAccessPointsRequest.Tag] = []
+            for v in value {
+                if v != nil {
+                    var model = DescribeCpfsAccessPointsRequest.Tag()
+                    if v != nil {
+                        model.fromMap(v as? [String: Any?])
+                    }
+                    tmp.append(model)
+                }
+            }
+            self.tag = tmp
         }
     }
 }
@@ -11485,6 +11605,44 @@ public class DescribeCpfsAccessPointsResponseBody : Tea.TeaModel {
                 }
             }
         }
+        public class Tags : Tea.TeaModel {
+            public var key: String?
+
+            public var value: String?
+
+            public override init() {
+                super.init()
+            }
+
+            public init(_ dict: [String: Any]) {
+                super.init()
+                self.fromMap(dict)
+            }
+
+            public override func validate() throws -> Void {
+            }
+
+            public override func toMap() -> [String : Any] {
+                var map = super.toMap()
+                if self.key != nil {
+                    map["Key"] = self.key!
+                }
+                if self.value != nil {
+                    map["Value"] = self.value!
+                }
+                return map
+            }
+
+            public override func fromMap(_ dict: [String: Any?]?) -> Void {
+                guard let dict else { return }
+                if let value = dict["Key"] as? String {
+                    self.key = value
+                }
+                if let value = dict["Value"] as? String {
+                    self.value = value
+                }
+            }
+        }
         public var ARN: String?
 
         public var accessPointId: String?
@@ -11502,6 +11660,8 @@ public class DescribeCpfsAccessPointsResponseBody : Tea.TeaModel {
         public var rootDirectory: DescribeCpfsAccessPointsResponseBody.AccessPoints.RootDirectory?
 
         public var status: String?
+
+        public var tags: [DescribeCpfsAccessPointsResponseBody.AccessPoints.Tags]?
 
         public override init() {
             super.init()
@@ -11545,6 +11705,13 @@ public class DescribeCpfsAccessPointsResponseBody : Tea.TeaModel {
             if self.status != nil {
                 map["Status"] = self.status!
             }
+            if self.tags != nil {
+                var tmp : [Any] = []
+                for k in self.tags! {
+                    tmp.append(k.toMap())
+                }
+                map["Tags"] = tmp
+            }
             return map
         }
 
@@ -11578,6 +11745,19 @@ public class DescribeCpfsAccessPointsResponseBody : Tea.TeaModel {
             }
             if let value = dict["Status"] as? String {
                 self.status = value
+            }
+            if let value = dict["Tags"] as? [Any?] {
+                var tmp : [DescribeCpfsAccessPointsResponseBody.AccessPoints.Tags] = []
+                for v in value {
+                    if v != nil {
+                        var model = DescribeCpfsAccessPointsResponseBody.AccessPoints.Tags()
+                        if v != nil {
+                            model.fromMap(v as? [String: Any?])
+                        }
+                        tmp.append(model)
+                    }
+                }
+                self.tags = tmp
             }
         }
     }
